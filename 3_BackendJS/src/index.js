4a5ff3f3d564c97f439040c1c6938a58c881bc3c
vinay961 <vinay100rai@gmail.com>
2024-01-31 20:53:59 +0530

    packages installation

--- 3_BackendJS/src/index.js
+++ 3_BackendJS/src/index.js
@@ -1,12 +1,21 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv'
+import express from 'express'
 import connectDB from "./db/dbConnection.js"
 dotenv.config({
     path:'./env'
 })
+const app = express()
 
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on PORT: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log(err);
+})
 
 
 
